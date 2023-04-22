c2b07a8e77dd45fccab37e1284bc17c20195c66d
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-23 01:44:18 +0300

    add count

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   const [text, setText] = useState("")
 
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000)
+  }, [])
 
   console.log("Component rendering");
 
